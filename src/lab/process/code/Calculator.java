69f156f34d14d235020fe271203e5ecc979bd269
Chathura De Silva <chathura.desilva@infor.com>
2018-07-13 21:16:33 +0530

    Added Calculator Basic Functionality

---
+++ src/lab/process/code/Calculator.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) SLIIT. All Rights Reserved.
+ */
+package lab.process.code;
+
+/**
+ * @author Chathura R De Silva
+ *
+ */
+public class Calculator {
+
+	/**
+	 * Adds two values
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
+	public int add(int value1, int value2) {
+		return value1 + value2;
+	}
+
+	/**
+	 * Subtracts second value from the first
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
+	public int substract(int value1, int value2) {
+		return value1 - value2;
+	}
+
+	/**
+	 * Multiply two values
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
+	public int multiply(int value1, int value2) {
+		return value1 * value2;
+	}
+
+	/**
+	 * Divide first value from the second
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
+	public int devide(int value1, int value2) {
+		return value1 / value2;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/lab/process/code/CalcuatorTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) SLIIT. All Rights Reserved.
+ */
+package lab.process.code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Chathura R De Silva
+ *
+ */
+class CalcuatorTest {
+
+	private Calculator calculator = null;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.calculator = new Calculator();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAdd() {
+		int value1 = 5;
+		int value2 = 3;
+		int answer;
+
+		answer = calculator.add(value1, value2);
+
+		assertEquals(answer, 8);
+	}
+
+	@Test
+	void testSubtract() {
+		int value1 = 5;
+		int value2 = 3;
+		int answer;
+
+		answer = calculator.substract(value1, value2);
+
+		assertEquals(answer, 2);
+	}
+
+	@Test
+	void testMultiply() {
+		int value1 = 5;
+		int value2 = 3;
+		int answer;
+
+		answer = calculator.multiply(value1, value2);
+
+		assertEquals(answer, 15);
+	}
+
+	@Test
+	void testDidide() {
+		int value1 = 5;
+		int value2 = 3;
+		int answer;
+
+		answer = calculator.devide(value1, value2);
+
+		assertEquals(answer, 1);
+	}
+
+	@Test
+	void testDivideZero() {
+		int value1 = 5;
+		int value2 = 0;
+		int answer;
+
+		try {
+			answer = calculator.devide(value1, value2);
+			assertNotNull(answer);
+		} catch (Exception e) {
+			assertTrue(e instanceof ArithmeticException);
+		}
+
+	}
+
+}
